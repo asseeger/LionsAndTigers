c138e9e83da40c235cc4bec15c0dcb5ac93be520
ASSeeger <asseeger@mac.com>
2014-09-20 07:21:57 +0200

    struct Tiger implemented

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Andreas Seeger @MBPR on 20.09.14.
+//  Copyright (c) 2014 ASS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var name = ""
+    var breed = ""
+    var age = 0
+    var image = UIImage(named: "")
+}
